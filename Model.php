e6f3f4e5fc06b1d9bbdf0283598e60902c72c7b9
R Sriranganathan <ranga96sricool@gmail.com>
2015-07-26 12:45:34 +0530

    added model

---
+++ Model.php
@@ -0,0 +1,32 @@
+<?php
+class Model
+{   
+	public $database;
+	public $table;
+	public function save()
+	{
+		$elements =get_object_vars( $this );
+		
+		$sql = "Insert into $this->table (";
+		foreach ($elements as $element=>$value)
+		if(strcmp($element,"database")&&strcmp($element,"table"))
+		$sql.=$element.",";
+		$sql = rtrim($sql, ",");
+		$sql.=") values (";
+		foreach ($elements as $element=>$value)
+		if(strcmp($element,"database")&&strcmp($element,"table"))
+		$sql.="\"".$value."\",";
+		$sql = rtrim($sql, ",");
+		$sql.=");";
+		$conn = new mysqli("localhost", "root","", $this->database);
+		$conn->query($sql);
+		
+	}
+	public function where($col,$cond,$value)
+	{
+		$conn = new mysqli("localhost", "root","", $this->database);
+		$sql = "SELECT * from $this->table where $col $cond \"$value\";";
+		return $conn->query($sql);
+	}
+}
+?>
